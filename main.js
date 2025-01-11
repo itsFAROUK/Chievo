cf41278d514e7c53c93e26b1b420a3fe1ef9fe7a
Farouk <bouazizfarouk3@gmail.com>
2025-01-11 23:01:22 +0100

    reinforced dark mode logic

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-// Dark mode toggle
-const darkModeToggle = document.querySelector('.dark-mode .toggle');
-darkModeToggle.addEventListener('click', _ => document.body.classList.toggle('dark-mode'));
+function initializeDarkMode() {
+    const darkModeBtn = document.querySelector('.dark-mode-btn')
+    const darkModeToggle = document.querySelector('.dark-mode-btn .toggle');
+    if (darkModeToggle) {
+        // Check localStorage or system for dark mode prefernce
+        const systemPreferenceDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        const savedPreference = localStorage.getItem('dark-mode');
+
+        if (savedPreference === 'enabled' || (systemPreferenceDark && savedPreference === null)) {
+            document.body.classList.add('dark-mode');
+            darkModeBtn.setAttribute('aria-pressed', 'true');
+        }
+        
+        darkModeToggle.addEventListener('click', () => {
+            const isDarkMode = document.body.classList.toggle('dark-mode');
+            darkModeBtn.setAttribute('aria-pressed', isDarkMode ? 'true' : 'false');
+            // Save preference to localStorage
+            localStorage.setItem('dark-mode', isDarkMode ? 'enabled' : 'disabled');
+        });
+    }
+}
+document.addEventListener('DOMContentLoaded', initializeDarkMode);
